8507b697d23e5a8041346fe282c23228d6d586c8
N.Madushan Kahingala <115458747+knmadushan@users.noreply.github.com>
2023-04-23 11:14:04 +0530

    Register

---
+++ register.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="register.css">
+    <title>Login</title>
+   
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="post">
+            <h2 class="text-center">Register</h2>
+            <div class="form-outline mb-3">
+                <label for="" class="form-label">First Name</label>
+                <input type="text" class="form-control" id="first1" name="f_name" placeholder="First Name">
+            </div>
+
+            <div class="form-outline mb-3">
+                <label for="" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="last1" name="l_nmae" placeholder="Last Name">
+            </div>
+
+            <div class="form-outline mb-3">
+                <label for="" class="form-label">Address</label>
+                <input type="text" class="form-control" id="add" name="address" placeholder="Address">
+            </div>
+            <div class="form-outline mb-3">
+                <label for="" class="form-label">E-mail</label>
+                <input type="mail" class="form-control" id="emain" name="mail" placeholder="E-mail">
+            </div>
+            <div class="button text-center">
+                <button type="submit" class="btn btn-primary btn-lg">Register</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
